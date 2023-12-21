f805389c05914ce6a8852b6ad63badfc81a173f3
Volodymyr Kozyr <vroger4@gmail.com>
2023-12-21 20:52:48 +0200

    HW 5 update
    HW 5 add Code Splitting (разделение кода)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 
 import Container from './Container/Container';
 import AppBar from './AppBar/AppBar';
 
-import FilmsList from 'pages/FilmsList/FilmsList';
-import Movies from '../pages/Movies/Movies';
-import MovieDetails from '../pages/MovieDetails/MovieDetails';
-import Cast from '../pages/Cast/Cast';
-import Reviews from '../pages/Reviews/Reviews';
-import NotFound from '../pages/NotFound/NotFound';
+const FilmsList = lazy(() => import('../pages/FilmsList/FilmsList'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('../pages/Cast/Cast'));
+const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
 
 export const App = () => {
   return (
     <Container>
       <AppBar />
-      <Routes>
-        <Route path="/" element={<FilmsList />}></Route>
-        <Route path="/movies" element={<Movies />}></Route>
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />}></Route>
-          <Route path="reviews" element={<Reviews />}></Route>
-        </Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<FilmsList />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />}></Route>
+            <Route path="reviews" element={<Reviews />}></Route>
+          </Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </Container>
   );
 };
